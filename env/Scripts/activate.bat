626e3c7867c82517acc3dee8286319c9f0919421
Ashfiqun Mustari <AshfiqunMustari@gmail.com>
2025-11-15 04:31:53 +0600

    project structure updated

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\2025\WEB DEVELOPMENT-SEPT25\Django\Course-2 (ML and Django)\Stock-Prediction-Portal\env
+set VIRTUAL_ENV=F:\2025\WEB-DEVELOPMENT-SEPT25\Django\Course-2-(ML and Django)\Stock-Prediction-Portal\env
 
 if not defined PROMPT set PROMPT=$P$G
 
